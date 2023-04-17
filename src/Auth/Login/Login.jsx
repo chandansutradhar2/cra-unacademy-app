9f7793ad13fa54b174dc6c15d3ef173af940057e
chandannaresh <chandansutradhar2@gmail.com>
2023-04-17 18:18:40 +0530

    added formik in login

--- src/Auth/Login/Login.jsx
+++ src/Auth/Login/Login.jsx
@@ -2,25 +2,25 @@ import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { Password } from "primereact/password";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import styles from "./login.module.css";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .required("Required")
+    .min(8, "Password must be 8 characters long"),
+});
 
 export const LoginForm = () => {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    validationSchema: LoginSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
   });
-  const [valid, isValid] = useState(false);
-
-  const validate = () => {
-    let isValid = true;
-    if (!user.email) {
-      isValid = false;
-    }
-    if (!user.password) {
-      isValid = false;
-      }
-      
-    return isValid;
-  };
 
   return (
     <div className="login">
@@ -28,38 +28,49 @@ export const LoginForm = () => {
         <div className="login__container__header">
           <h1>Login</h1>
         </div>
-        <div className="login__container__body">
+        <form className="login__container__body" onSubmit={formik.handleSubmit}>
           <div className="p-inputgroup" style={{ marginBottom: "1rem" }}>
             <span className="p-inputgroup-addon">
               <i className="pi pi-user"></i>
             </span>
             <InputText
               placeholder="Email"
-              value={user.email}
-              onChange={(ev) => {
-                setUser({ ...user, email: ev.target.value });
-              }}
+              id="email"
+              name="email"
+              type="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              className={formik.errors.email ? "p-invalid block" : ""}
             />
           </div>
+          {formik.errors.email ? (
+            <small className={styles.password}>{formik.errors.email}</small>
+          ) : null}
           <div className="p-inputgroup" style={{ marginBottom: "1rem" }}>
             <span className="p-inputgroup-addon">
               <i className="pi pi-key"></i>
             </span>
             <Password
               placeholder="Password"
-              value={user.password}
-              onChange={(ev) => {
-                setUser({ ...user, password: ev.target.value });
-              }}
+              value={formik.values.password}
+              feedback={false}
+              toggleMask={true}
+              id="password"
+              name="password"
+              onChange={formik.handleChange}
+              className={formik.errors.password ? "p-invalid block" : ""}
             />
           </div>
+          {formik.errors.password ? (
+            <small className={styles.password}>{formik.errors.password}</small>
+          ) : null}
           <div className="p-inputgroup" style={{ marginBottom: "1rem" }}>
             <span className="p-inputgroup-addon">
               <i className="pi pi-arrow-right"></i>
             </span>
-            <Button label="Login" onClick={validate} />
+            <Button label="Login" type="submit" />
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
