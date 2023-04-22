55293708b96d3e3e54a32553ba90f418a2cc40a4
chandannaresh <chandansutradhar2@gmail.com>
2023-04-22 13:16:13 +0530

    added section form and list

---
+++ src/Courses/Section/SectionForm.jsx
@@ -0,0 +1,8 @@
+export function SectionForm() {
+    return (
+        <div>
+            <h1>Section Form</h1>
+        </div>
+        
+    )
+}
---
+++ src/Courses/Section/SectionList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function SectionList() {
+    return (
+        <div>
+            <h1>Section List</h1>
+        </div>
+    )
+}
